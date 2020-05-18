7edc987dd7c7840fda490fa3ec7bbc98409ca52c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-18 23:59:24 +0000

    Update SplashScreen.java

--- app/src/main/java/com/example/myzone/SplashScreen.java
+++ app/src/main/java/com/example/myzone/SplashScreen.java
@@ -1,4 +1,22 @@
 package com.example.myzone;
 
-public class SplashScreen {
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
 }

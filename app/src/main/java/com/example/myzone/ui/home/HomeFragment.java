6508cb8b5f42775f2fb17ccb22223aed560d2c48
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-19 01:52:42 +0000

    Update HomeFragment.java

--- app/src/main/java/com/example/myzone/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myzone/ui/home/HomeFragment.java
@@ -13,6 +13,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myzone.R;
+import com.example.myzone.scarping.WordsAdapter;
 
 import java.util.Collections;
 import java.util.Comparator;

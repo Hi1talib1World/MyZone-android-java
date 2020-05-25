fffbe64f4015c20fb53aa4c595f6e77e0de64c65
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-25 08:26:59 +0000

    Update HomeFragment.java

--- app/src/main/java/com/example/myzone/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myzone/ui/home/HomeFragment.java
@@ -45,6 +45,7 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
     Spinner spinner;
 
+
     Yesterday ytr;
     Tomorrow tmr;
     DayAfterTomorrow dayAfterTomorrow;

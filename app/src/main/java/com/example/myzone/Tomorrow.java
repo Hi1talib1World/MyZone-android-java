51e54384fd39dab62c940611c797fc9755ca0965
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-25 08:29:41 +0000

    Update Tomorrow.java

--- app/src/main/java/com/example/myzone/Tomorrow.java
+++ app/src/main/java/com/example/myzone/Tomorrow.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,11 @@ import android.view.ViewGroup;
 
 
 public class Tomorrow extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

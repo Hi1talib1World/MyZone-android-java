336cb4faeeb8b31a9b9d93041126be3e63beb204
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-10 20:59:08 +0100

    Create channel.java

---
+++ app/src/main/java/com/example/myzone/selected_channels/channel.java
@@ -0,0 +1,9 @@
+package com.example.myzone.selected_channels;
+
+public class channel {
+    private Long id;
+    private String name;
+    private String emailAddress;
+    private int imageId;
+    private String imagePath;
+}

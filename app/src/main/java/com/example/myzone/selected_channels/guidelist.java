4fdb6f7ea277c66db31b8594e81d4b0fdc1ab2df
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-10 20:49:53 +0100

    Create guidelist.java

---
+++ app/src/main/java/com/example/myzone/selected_channels/guidelist.java
@@ -0,0 +1,14 @@
+package com.example.myzone.selected_channels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class guidelist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guidelist);
+    }
+}

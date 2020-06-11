6f3b6cd544e6b15b42b2dfaa9f5dc77b99772db7
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-11 14:50:54 +0100

    Update guidelist.java

--- app/src/main/java/com/example/myzone/selected_channels/guidelist.java
+++ app/src/main/java/com/example/myzone/selected_channels/guidelist.java
@@ -1,12 +1,22 @@
 package com.example.myzone.selected_channels;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.myzone.R;
 
+import java.util.List;
+
 public class guidelist extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private channelsListAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ItemTouchHelper mItemTouchHelper;
+    private List<channel> mCustomers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

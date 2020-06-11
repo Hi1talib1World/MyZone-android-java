70334d4e67d54d1b6f1575a43ecfbf70a5aaa02c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-11 22:32:33 +0100

    Update ViewPagerAdapter.java

--- app/src/main/java/com/example/myzone/theshow/ViewPagerAdapter.java
+++ app/src/main/java/com/example/myzone/theshow/ViewPagerAdapter.java
@@ -1,13 +1,20 @@
 package com.example.myzone.theshow;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.myzone.R;
+
 import java.util.List;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
-    private final List<Fragment> mmFragmentTitleList FragmentList = new ArrayList<>();
+    private final List<Fragment>mmFragmentTitleList FragmentList = new ArrayList<>();
     private final List<String> = new ArrayList<>();
 
     public ViewPagerAdapter(FragmentManager manager) {
@@ -32,5 +39,29 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         return mFragmentTitleList.get(position);
+    }
+    try {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.header);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @SuppressWarnings("ResourceType")
+            @Override
+            public void onGenerated(Palette palette) {
+
+                int vibrantColor = palette.getVibrantColor(R.color.primary_500);
+                int vibrantDarkColor = palette.getDarkVibrantColor(R.color.primary_700);
+                collapsingToolbarLayout.setContentScrimColor(vibrantColor);
+                collapsingToolbarLayout.setStatusBarScrimColor(vibrantDarkColor);
+            }
+        });
+
+    } catch (Exception e) {
+        // if Bitmap fetch fails, fallback to primary colors
+        Log.e(TAG, "onCreate: failed to create bitmap from background", e.fillInStackTrace());
+        collapsingToolbarLayout.setContentScrimColor(
+                ContextCompat.getColor(this, R.color.primary_500)
+        );
+        collapsingToolbarLayout.setStatusBarScrimColor(
+                ContextCompat.getColor(this, R.color.primary_700)
+        );
     }
 }

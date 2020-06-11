a68a795f738a9c5ba417b0aa2f338f5f0eae5ef5
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-11 15:17:59 +0100

    Update SharedPreference.java

--- app/src/main/java/com/example/myzone/selected_channels/util/SharedPreference.java
+++ app/src/main/java/com/example/myzone/selected_channels/util/SharedPreference.java
@@ -1,7 +1,15 @@
 package com.example.myzone.selected_channels.util;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import com.example.myzone.selected_channels.channel;
+import com.google.gson.Gson;
+
+import java.nio.channels.Channel;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 At this point, your drag and drop list should be working and we now want to
@@ -19,6 +27,29 @@ public class SharedPreference  {
     public final static String PREFERENCE_FILE = "preference_file";
 
     public void onCreat(Bundle savedInstanceState){
+        mSharedPreferences = this.getApplicationContext()
+                .getSharedPreferences(PREFERENCE_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+    }
+    @Override
+    public void onNoteListChanged(List<channel> customers) {
+        //after drag and drop operation, the new list of Customers is passed in here
 
+        //create a List of Long to hold the Ids of the
+        //Customers in the List
+        List<Long> listOfSortedCustomerId = new ArrayList<Long>();
+
+        for (Channel channel: customers){
+            listOfSortedCustomerId.add(channel.getId());
+        }
+
+        //convert the List of Longs to a JSON string
+        Gson gson = new Gson();
+        String jsonListOfSortedCustomerIds = gson.toJson(listOfSortedCustomerId);
+
+
+        //save to SharedPreference
+        mEditor.putString(LIST_OF_SORTED_DATA_ID, jsonListOfSortedCustomerIds).commit();
+        mEditor.commit();
     }
 }

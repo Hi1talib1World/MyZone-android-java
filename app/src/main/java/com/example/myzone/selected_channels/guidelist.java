ffe2d6f5f88c35b73b352e25eb1a2514240e3ff7
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-11 15:20:06 +0100

    Update guidelist.java

--- app/src/main/java/com/example/myzone/selected_channels/guidelist.java
+++ app/src/main/java/com/example/myzone/selected_channels/guidelist.java
@@ -11,9 +11,14 @@ import com.example.myzone.R;
 import com.example.myzone.selected_channels.util.SimpleItemTouchHelperCallback;
 import com.example.myzone.selected_channels.util.listener.OnCustomerListChangedListener;
 import com.example.myzone.selected_channels.util.listener.OnStartDragListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.myzone.selected_channels.util.SharedPreference.LIST_OF_SORTED_DATA_ID;
 
 public class guidelist extends AppCompatActivity implements OnCustomerListChangedListener,
         OnStartDragListener {
@@ -48,6 +53,52 @@ public class guidelist extends AppCompatActivity implements OnCustomerListChange
                 .size(2)
                 .build());
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    private List<channel> getSampleData(){
+
+        //Get the sample data
+        List<channel> customerList = SampleData.addSampleCustomers();
+
+        //create an empty array to hold the list of sorted Customers
+        List<channel> sortedCustomers = new ArrayList<channel>();
+
+        //get the JSON array of the ordered of sorted customers
+        String jsonListOfSortedCustomerId = mSharedPreferences.getString(LIST_OF_SORTED_DATA_ID, "");
+
+        //check for null
+        if (!jsonListOfSortedCustomerId.isEmpty()){
+
+            //convert JSON array into a List<Long>
+            Gson gson = new Gson();
+            List<Long> listOfSortedCustomersId = gson.fromJson
+                    (jsonListOfSortedCustomerId, new TypeToken<List<Long>>(){}.getType());
+
+            //build sorted list
+            if (listOfSortedCustomersId != null && listOfSortedCustomersId.size() > 0){
+                for (Long id: listOfSortedCustomersId){
+                    for (channel channel: customerList){
+                        if (channel.getId().equals(id)){
+                            sortedCustomers.add(channel);
+                            customerList.remove(channel);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //if there are still customers that were not in the sorted list
+            //maybe they were added after the last drag and drop
+            //add them to the sorted list
+            if (customerList.size() > 0){
+                sortedCustomers.addAll(customerList);
+            }
+
+            return sortedCustomers;
+        }else {
+            return customerList;
+        }
     }
 
     @Override

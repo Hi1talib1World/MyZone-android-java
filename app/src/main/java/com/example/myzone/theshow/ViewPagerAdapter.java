af2e44ba3311bc63b5fa916e46403115c36e0202
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-11 22:14:26 +0100

    Create ViewPagerAdapter.java

---
+++ app/src/main/java/com/example/myzone/theshow/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.myzone.theshow;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mmFragmentTitleList FragmentList = new ArrayList<>();
+    private final List<String> = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}

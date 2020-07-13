86322d831631e8a66e80b4c3d87315101a1c1aa2
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-13 16:03:29 +0100

    Update NotificationsViewModel.java

--- app/src/main/java/com/example/myzone/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/myzone/ui/notifications/NotificationsViewModel.java
@@ -1,12 +1,52 @@
 package com.example.myzone.ui.notifications;
 
+import android.view.Gravity;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.synnapps.carouselview.CarouselViewPager;
+import com.synnapps.carouselview.CirclePageIndicator;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
+import com.synnapps.carouselview.ViewListener;
+
+import java.util.Timer;
+
 public class NotificationsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+
+    private final int DEFAULT_GRAVITY = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+
+    private static final int DEFAULT_SLIDE_INTERVAL = 3500;
+    private static final int DEFAULT_SLIDE_VELOCITY = 400;
+    public static final int DEFAULT_INDICATOR_VISIBILITY = 0;
+
+
+    private int mPageCount;
+    private int slideInterval = DEFAULT_SLIDE_INTERVAL;
+    private int mIndicatorGravity = DEFAULT_GRAVITY;
+    private int indicatorMarginVertical;
+    private int indicatorMarginHorizontal;
+    private int pageTransformInterval = DEFAULT_SLIDE_VELOCITY;
+    private int indicatorVisibility = DEFAULT_INDICATOR_VISIBILITY;
+
+    private CarouselViewPager containerViewPager;
+    private CirclePageIndicator mIndicator;
+    private ViewListener mViewListener = null;
+    private ImageListener mImageListener = null;
+    private ImageClickListener imageClickListener = null;
+
+    private Timer swipeTimer;
+    private SwipeTask swipeTask;
+
+    private boolean autoPlay;
+    private boolean disableAutoPlayOnUserInteraction;
+    private boolean animateOnBoundary = true;
+
+    private int previousState;
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();

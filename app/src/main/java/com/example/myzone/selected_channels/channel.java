d89de4f81bebd1b1438b1ad35cbb33147f4b7da5
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-08-07 11:32:41 +0100

    Update channel.java

--- app/src/main/java/com/example/myzone/selected_channels/channel.java
+++ app/src/main/java/com/example/myzone/selected_channels/channel.java
@@ -6,7 +6,7 @@ public class channel {
     private String emailAddress;
     private int imageId;
     private String imagePath;
-}
+
     public channel() {
     }
     public channel(Long id, String name,String emailAddress ,int imageId ,String imagePath ) {
@@ -17,3 +17,44 @@ public class channel {
         this.id=id;
 
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
